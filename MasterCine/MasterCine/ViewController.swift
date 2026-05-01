7db49ef149e732066f5cc703a71339b72f89ed45
Gabriel Merenfeld <gabrielmerenfeld@gmail.com>
2026-05-01 08:04:13 -0300

    feat: create project in ViewCode

---
+++ MasterCine/MasterCine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MasterCine
+//
+//  Created by Gabriel Merenfeld on 01/05/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
