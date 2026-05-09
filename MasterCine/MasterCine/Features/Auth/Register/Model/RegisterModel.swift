e7e96d5006b0ccbbff750e99d7a82e9ca1fa95bd
Gabriel Merenfeld <gabrielmerenfeld@gmail.com>
2026-05-09 10:51:31 -0300

    feat: create RegisterModel file in Features/Auth/Register/Model folder

---
+++ MasterCine/MasterCine/Features/Auth/Register/Model/RegisterModel.swift
@@ -0,0 +1,13 @@
+//
+//  RegisterModel.swift
+//  MasterCine
+//
+//  Created by Gabriel Merenfeld on 09/05/26.
+//
+
+struct RegisterModel {
+    var name: String = ""
+    var email: String = ""
+    var password: String = ""
+    var confirmPassword: String = ""
+}
