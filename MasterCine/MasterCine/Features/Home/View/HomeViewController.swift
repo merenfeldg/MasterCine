c61761181e89fe18530563389bef37dcec6919bd
Gabriel Merenfeld <gabrielmerenfeld@gmail.com>
2026-05-21 08:26:02 -0300

    feat: create HomeViewController

--- MasterCine/MasterCine/Features/Home/View/HomeViewController.swift
+++ MasterCine/MasterCine/Features/Home/View/HomeViewController.swift
@@ -8,5 +8,28 @@
 import UIKit
 
 final class HomeViewController: UIBaseViewController {
+    let screen: HomeScreen = HomeScreen()
     
+    override func loadView() {
+        view = screen
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    private func configProtocols() {
+        screen.configTableViewProtocols(delegate: self, dataSource: self)
+    }
+}
+
+extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 }
